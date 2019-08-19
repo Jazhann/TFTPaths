53b429ede07465477a2ea2cac58acdd317b1c913
Daniel Rodriguez <danirg@gmail.com>
2019-08-19 20:09:13 +0200

    Changes in port

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const path = require('path');
 
 var app = express();
-// Start the app by listening on the default Heroku port
-var port = 8080;
 
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/dist/assets'));
@@ -12,7 +10,7 @@ app.get('*',function(req,res){
     res.sendFile(__dirname + '/dist/index.html');
 });
 
-app.listen(port, function()
+app.listen(8080, function()
 {
     console.log("ok");
 });
