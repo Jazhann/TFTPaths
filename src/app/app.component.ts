50cb05d1915cc90e14883255cfb213545c59fb89
Daniel Rodriguez <danirg@gmail.com>
2021-09-29 18:15:02 +0200

    Added SEO data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TFTPaths';
+
+  constructor(
+    private titleService: Title,
+    private metaService: Meta
+  ){}
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Teamfight Tactics'},
+      {name: 'description', content: 'Teamfight Tactics champions sinergies and combinations.'},
+      {name: 'robots', content: 'index, follow'}
+    ]);
+  }
 }
